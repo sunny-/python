ab28e5771428c690421d01c086feea5714bbd636
sunny- <shailesh.upadhyay17@gmail.com>
2016-10-10 15:42:07 +0530

    practice problems

--- Leap year.py
+++ Leap year.py
@@ -1,13 +1,24 @@
 # Finding if a given year is a leap year or not
+##
+##y = 1600
+##if y%4 == 0:
+##    if y%100 == 0:
+##        if y%400 == 0:
+##            print ('Leap year')
+##        else:
+##            print ('Not a leap year')      
+##    else:
+##        print ('Leap year')
+##else:
+##    print ('Not a leap year')
 
-y = 1600
-if y%4 == 0:
-    if y%100 == 0:
-        if y%400 == 0:
-            print ('Leap year')
-        else:
-            print ('Not a leap year')      
-    else:
-        print ('Leap year')
+
+
+y = 1664
+if y%4 == 0 and y%100 != 0:
+    print ('Leap year')
+elif y%4 == 0 and y%100 == 0 and y%400 == 0:
+    print ('Leap year')
 else:
     print ('Not a leap year')
+
---
+++ name _check.py
@@ -0,0 +1,11 @@
+#write a program that take names from user and checks it.
+#It checks for 3 times befor quitting i.e it takes 3 guesses
+#unfinished
+
+name = input("Enter your name")
+
+if name=='Raj':
+    print('Welcome Raj')
+
+
+
