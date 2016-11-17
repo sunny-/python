27d3f47b1a89dc405afbf7e7d4d9e07c89bc4f9d
sunny- <shailesh.upadhyay17@gmail.com>
2016-11-17 18:08:07 +0530

    match

---
+++ fib_fast.py
@@ -0,0 +1,21 @@
+numCalls = 0
+def fib(n): # fibbonacci without memoisation
+    global numCalls
+    numCalls += 1
+    print ('fib called with', n)
+    if n <= 1: return n
+    else: return fib(n-1) + fib(n-2)
+
+
+def fib1(n): # fibbonacci with memoisation
+    memo = {0:0, 1:1}
+    return fastFib(n, memo)
+
+ 
+def fastFib(n, memo):
+    global numCalls
+    numCalls += 1
+    print ('fib1 called with', n)
+    if not n in memo:
+            memo[n] = fastFib(n-1, memo) + fastFib(n-2, memo)
+    return memo[n]
