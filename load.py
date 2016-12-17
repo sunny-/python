279252415a315c77defbea51dd462ec39cad06c5
sunny- <shailesh.upadhyay17@gmail.com>
2016-12-17 19:41:03 +0530

    json

--- load.py
+++ load.py
@@ -8,26 +8,30 @@ time_zones = [rec['tz'] for rec in records if 'tz' in rec]
 
 
 
-def tz_counts(seq):
-    dics = {}
-    for i in seq:
-        if i in dics:
-            dics[i] += 1
-
-        else:
-            dics[i] = 1
-    return dics
-
-counts = tz_counts(time_zones)
-
-
-
-
-def top_counts(count_dict,n=10):
-
-    value_key_pairs = [(count,tz)for tz, count in count_dict.items()]
-    value_key_pairs.sort()
-    return value_key_pairs[-n:]
+##def tz_counts(seq):
+##    dics = {}
+##    for i in seq:
+##        if i in dics:
+##            dics[i] += 1
+##
+##        else:
+##            dics[i] = 1
+##    return dics
+##
+##counts = tz_counts(time_zones)
+##
+##
+##
+##
+##def top_counts(count_dict,n=10):
+##
+##    value_key_pairs = [(count,tz)for tz, count in count_dict.items()]
+##    value_key_pairs.sort()
+##    return value_key_pairs[-n:]
 
     
-    
+from collections import Counter
+
+counts = Counter(time_zones)
+
+print(counts.most_common(10))
