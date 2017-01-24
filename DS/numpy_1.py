432a81cae4e590067419c407c95e76519add0015
sunny- <shailesh.upadhyay17@gmail.com>
2017-01-24 19:58:53 +0530

    numpy

---
+++ DS/numpy_1.py
@@ -0,0 +1,12 @@
+import numpy as np
+# p = np.array([range(i,i+3)for i in [2,4,6]])
+# print (p)
+# print(type (p[0]))
+
+print ((np.zeros(10,dtype=int)))
+
+print ((np.ones((3,5),dtype=float)))
+
+print (np.full((3,5),3.14))
+
+
