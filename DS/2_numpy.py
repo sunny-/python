6e3046c91facab05a5778b2f67987795507006aa
sunny- <shailesh.upadhyay17@gmail.com>
2017-01-25 14:36:55 +0530

    array

--- DS/2_numpy.py
+++ DS/2_numpy.py
@@ -1,9 +1,1 @@
-x1 = np.random.normal(0, 0.8, 1000)
-x2 = np.random.normal(-2, 1, 1000)
-x3 = np.random.normal(3, 2, 1000)
 
-kwargs = dict(histtype='stepfilled', alpha=0.3, normed=True, bins=40)
-
-plt.hist(x1, **kwargs)
-plt.hist(x2, **kwargs)
-plt.hist(x3, **kwargs);
--- DS/numpy_1.py
+++ DS/numpy_1.py
@@ -3,10 +3,46 @@ import numpy as np
 # print (p)
 # print(type (p[0]))
 
-print ((np.zeros(10,dtype=int)))
+# print ((np.zeros(10,dtype=int)))
 
-print ((np.ones((3,5),dtype=float)))
+# print ((np.ones((3,5),dtype=float)))
 
-print (np.full((3,5),3.14))
+# print (np.full((3,5),3.14))
+
+
+np.random.seed(0) # seed for reproducibility
+x1 = np.random.randint(10, size = 6)
+x2 = np.random.randint(10, size = (3,4))
+x3 = np.random.randint(10, size = (3,4,5))
+
+print (x3)
+
+
+print("x3 ndim: ", x3.ndim)
+print("x3 shape:", x3.shape)
+print("x3 size: ", x3.size)
+
+print("x3 dtype: ", x3.dtype)
+
+print("x3 itemsize: ", x3.itemsize, "bytes")
+
+print("nbytes: ", x3.nbytes, "bytes")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
