f8298dafbc677653f65f3f2f71ff060e4fe7d03b
sunny- <shailesh.upadhyay17@gmail.com>
2017-01-28 11:18:45 +0530

    ufuncs

--- DS/numpy_345.py
+++ DS/numpy_345.py
@@ -1,15 +1,56 @@
 import numpy as np
-np.random.seed(0)
+# np.random.seed(0)
 
-def comp_reci(values):
-	output = np.empty(len(values))
-	for i in range(len(values)):
-		output[i] = 1.0/values[i]
-	print (output)
-	return output
+# def comp_reci(values):
+# 	output = np.empty(len(values))
+# 	for i in range(len(values)):
+# 		output[i] = 1.0/values[i]
+# 	print (output)
+# 	return output
 
-values = np.random.randint(1,10,size=5)
+# values = np.random.randint(1,10,size=5)
 
-# print(values)
+# # print(values)
 
-comp_reci(values)
+# comp_reci(values)
+
+# print (1.0/values)
+
+# a = np.arange(5)/np.arange(1,6)
+
+# print (a)
+
+x = np.arange(9).reshape((3,3))
+p = 2**x
+
+print(p)
+y = np.arange(9)
+
+print(y)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
