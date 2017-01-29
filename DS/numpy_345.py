b04bdf149f6fa749b47d60c173e876db9464aedd
sunny- <shailesh.upadhyay17@gmail.com>
2017-01-29 09:51:42 +0530

    aggregates

--- DS/numpy_345.py
+++ DS/numpy_345.py
@@ -1,52 +1,52 @@
 import numpy as np
-# np.random.seed(0)
+np.random.seed(0)
 
-# def comp_reci(values):
-# 	output = np.empty(len(values))
-# 	for i in range(len(values)):
-# 		output[i] = 1.0/values[i]
-# 	print (output)
-# 	return output
+def comp_reci(values):
+	output = np.empty(len(values))
+	for i in range(len(values)):
+		output[i] = 1.0/values[i]
+	print (output)
+	return output
 
-# values = np.random.randint(1,10,size=5)
+values = np.random.randint(1,10,size=5)
 
-# # print(values)
+# print(values)
 
-# comp_reci(values)
+comp_reci(values)
 
-# print (1.0/values)
+print (1.0/values)
 
-# a = np.arange(5)/np.arange(1,6)
+a = np.arange(5)/np.arange(1,6)
 
-# print (a)
+print (a)
 
-# x = np.arange(9).reshape((3,3))
-# p = 2**x
+x = np.arange(9).reshape((3,3))
+p = 2**x
 
-# print(p)
-# y = np.arange(9)
-# q= y+2
-# print(q)
+print(p)
+y = np.arange(9)
+q= y+2
+print(q)
 
-# x = np.array([-2,-1,0,1,2])
-# print (abs(x))
+x = np.array([-2,-1,0,1,2])
+print (abs(x))
 
-# y = np.array([3-4j,4-3j])
-# print (abs(y))
+y = np.array([3-4j,4-3j])
+print (abs(y))
 
-# th = np.linspace(0,np.pi,3)
+th = np.linspace(0,np.pi,3)
 
-# print("theta =", th)
-# print("sin(theta) =",np.sin(th))
+print("theta =", th)
+print("sin(theta) =",np.sin(th))
 
-# p = [1,2,3]
-# print("p =", p)
-# print("e^p =", np.exp(p))
+p = [1,2,3]
+print("p =", p)
+print("e^p =", np.exp(p))
 
-# from scipy import special
+from scipy import special
 
-# x = [1,5,10]
-# print("gamma(x) =", special.gamma(x))
+x = [1,5,10]
+print("gamma(x) =", special.gamma(x))
 
 x = np.arange(5)
 y = np.empty(5)
@@ -59,6 +59,21 @@ y = np.zeros(10)
 np.power(2,x,out=y[::2])
 
 print(y)
+
+
+x = np.arange(1,6)
+print(np.add.reduce(x))
+
+print(np.multiply.reduce(x))
+
+print(np.add.accumulate(x))
+
+print(np.multiply.accumulate(x))
+
+
+x = np.arange(1,6)
+print(np.multiply.outer(x,x))
+
 
 
 
