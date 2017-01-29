2ac73ee0227a15011b97c30cb6189af73e59815e
sunny- <shailesh.upadhyay17@gmail.com>
2017-01-29 09:35:58 +0530

    ufuncs

--- DS/numpy_345.py
+++ DS/numpy_345.py
@@ -24,9 +24,29 @@ import numpy as np
 # p = 2**x
 
 # print(p)
-y = np.arange(9)
-q= y+2
-print(q)
+# y = np.arange(9)
+# q= y+2
+# print(q)
+
+# x = np.array([-2,-1,0,1,2])
+# print (abs(x))
+
+# y = np.array([3-4j,4-3j])
+# print (abs(y))
+
+th = np.linspace(0,np.pi,3)
+
+print("theta =", th)
+print("sin(theta) =",np.sin(th))
+
+p = [1,2,3]
+print("p =", p)
+print("e^p =", np.exp(p))
+
+
+
+
+
 
 
 
