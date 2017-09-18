791df4027684d4fbf261e33dfea354ebea27c8b2
sunny- <shailesh.upadhyay17@gmail.com>
2017-09-18 17:45:45 +0530

    m

---
+++ Cl.py
@@ -0,0 +1,21 @@
+class Staff601():
+    room = "304"
+
+    def __init__(self,greeting):
+        self.greeting = greeting
+
+    def sayHi(self):
+        print (self.greeting)
+
+
+sunny = Staff601('hi')
+
+uj = Staff601('hello')
+
+sunny.sayHi()
+
+uj.sayHi()
+
+sunny.room
+
+print (uj.room)
