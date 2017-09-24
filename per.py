af508895b195e2602414781ce329540350c90067
sunny- <shailesh.upadhyay17@gmail.com>
2017-09-24 19:24:00 +0530

    cl

---
+++ per.py
@@ -0,0 +1,19 @@
+class Person:
+
+    def __init__(self,first_name,last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+
+
+sunny = Person('sh','upa')
+
+
+class Cartesian:
+
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+
+point_1 = Cartesian(0.1,0.2)
--- sq.py
+++
@@ -1,8 +0,0 @@
-x=4
-y=0
-i=0
-while(i<x):
-    y=x+y
-    i=i+1
-
-print y
