b80fe1c1ef48cdd49bb90277a28a350670b709b1
sunny- <shailesh.upadhyay17@gmail.com>
2017-11-02 06:44:59 +0530

    Ufuncs

--- DS/Single_var_reg_profits.py
+++ DS/Single_var_reg_profits.py
@@ -7,4 +7,10 @@ plt.scatter(vals[:,0],vals[:,1])
 plt.title('Graph')
 plt.xlabel('Population')
 plt.ylabel('Profits') 
-plt.show()
+# plt.show()
+# profit_data.plot(kind='scatter', x='Population', y='Profits')
+# plt.scatter('Population','Profits')
+# profit_data['theta0'] = 1
+# cf = np.zeros(shape = (2,1)) # Value of Xo
+# iteration = 1500
+# alpha = 0.01 
